f1ac69d734df8976dfc345852f3f9f0dbd1946da
Кирилл Аксенов <kirill.ax@tprofeng.ru>
2022-03-21 11:08:15 +0300

    Добавьте файлы проекта.

---
+++ Task6WpfApp/WeatherControl.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Task6WpfApp
+{
+    enum Precipitation
+    {
+        Sunny,
+        Cloudy,
+        Rain,
+        Snow
+    }
+    class WeatherControl:DependencyObject
+    {
+        public static readonly DependencyProperty TemperatureProperty;
+        private string windDirection;
+        private int windSpeed;
+        private Precipitation precipitation;
+
+        public int Temperature
+        {
+            get => (int)GetValue(TemperatureProperty);
+
+            set => SetValue(TemperatureProperty, value);
+        }
+
+        public string WindDirection
+        {
+            get => windDirection;
+            set => windDirection = value;
+        }
+
+        public int WindSpeed
+        {
+            get => windSpeed;
+            set => windSpeed = value;
+        }        
+
+        public WeatherControl(int temperature, string windDirection, int windSpeed, Precipitation precipitation)
+        {
+            this.Temperature = temperature;
+            this.WindDirection = windDirection;
+            this.WindSpeed = windSpeed;
+            this.precipitation = precipitation;
+        }
+
+        static WeatherControl()
+        {
+            TemperatureProperty = DependencyProperty.Register(
+                nameof(Temperature),
+                typeof(int),
+                typeof(WeatherControl),
+                new FrameworkPropertyMetadata(
+                    0,
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.AffectsRender,
+                    null,
+                    new CoerceValueCallback(CoerceTemperature)),
+                new ValidateValueCallback(ValidateTemperature));
+        }
+
+        private static bool ValidateTemperature(object value)
+        {
+            int v = (int) value;
+            if (v >= -50 && v <= 50)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        private static object CoerceTemperature(DependencyObject d, object baseValue)
+        {
+            int v = (int)baseValue;
+            if (v >= -50)
+            {
+                return v;
+            }
+            else
+                return 0;
+        }
+
+        public string Print()
+        {
+            return $"{Temperature} {WindDirection} {WindSpeed} {precipitation}";
+        }
+    }
+}
